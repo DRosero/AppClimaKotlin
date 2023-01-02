6c48390d156c2519dda511ec3e8f9eccbfb6c679
DRosero <die_go_ja_vier@yahoo.com>
2023-01-02 00:17:02 -0500

    interfaz creada y personalizada

---
+++ app/src/main/java/com/aplicacion/appclima/Activity_ciudad.kt
@@ -0,0 +1,11 @@
+package com.aplicacion.appclima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_ciudad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciudad)
+    }
+}
